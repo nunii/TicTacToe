b9ac22efe0af2cae7094c55742e55a7f12d52b39
BarJan <33749378+BarJan@users.noreply.github.com>
2018-05-09 21:41:43 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,8 +1,7 @@
 #ifndef BOARD_H
 #define BOARD_H
-#include "X_O.h"
+#include "XO.h"
 using namespace std;
-#endif
 
     class Board{
         
@@ -15,45 +14,28 @@ using namespace std;
             
             inline int getLength() const {return this -> length;}
             inline pairs getPair() const {return this -> place;}
-            inline void reset(){
-                for(int i=0; i<this ->getLength() ; i++){
-                    for(int j=0; j<this ->getLength(); j++){
-                        board[i][j].setDot();
-                    }
-                }
-            }
-            
             inline void fill(char c, int i, int j){
               
                 board[i][j].setCurr(c);
                 
             }
             
-            Board(int);
-            Board(const Board&);
+            Board(int len);
+            Board(const Board& other);
             Board();
            // ~Board();
-           friend ostream& operator<<(ostream& , Board&);
-          
-            Board& operator=(const Board&);
-            char operator=(char);
+           friend ostream& operator<<(ostream& out, Board& b);
+           void reset();
+            Board& operator=(const Board& other);
+            char operator=(char c);
            
-            X_O& operator[](const pairs);
+            X_O& operator[](const pairs pair);
             
 
           /*  string toString()const;*/
     };
     
-inline ostream& operator<<(ostream& out, Board& b)
-{
-   for(int i = 0; i < b.getLength(); i++){
-			for(int j = 0; j < b.getLength(); j++ ){
-				out << (b.board[i][j]).getChar();
-			}
-			out << endl;
-		}
-        return out;
-}
+
  
     
-   
+#endif
