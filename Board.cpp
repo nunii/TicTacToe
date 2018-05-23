c0cb9d29b18a45e49c1b3c275e94209919852924
nunii <33713479+nunii@users.noreply.github.com>
2018-05-24 01:25:01 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -106,8 +106,7 @@ char Board :: operator=(char c){
 
 X_O& Board :: operator[](const Coordinate c)const
 {
-  if((c.i >= this -> size() || c.j >= this -> size()) ||
-     (c.i < 0 || c.j <0))
+  if((c.i >= this -> size() || c.j >= this -> size())
      {
       throw (IllegalCoordinateException(c));
    }
